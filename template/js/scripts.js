d6a3dd4d850a4d1312bc96a4893e10cc3966e6fe
sharper <hi@sammyharper.com>
2025-08-21 16:30:17 -0500

    feat!: init 11ty template

--- template/js/scripts.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", function(){
-
-  console.log(`[${window.siteData.project}] scripts.js loaded`);
-
-  // Add a class to the body element to indicate that the page has loaded
-  document.body.classList.add("loaded");
-  
-}, false);
